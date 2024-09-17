a637cb983713fc522aec08c7332d0c33c9609c22
EsterHad146 <hadassaester146@gmail.com>
2024-09-17 16:10:18 -0300

    Add files via upload

---
+++ prisma/migrations/20240917173733_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Fornecedor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `cnpj` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `telefone` VARCHAR(191) NOT NULL,
+    `endereco` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Fornecedor_cnpj_key`(`cnpj`),
+    UNIQUE INDEX `Fornecedor_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Produto` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `codBarras` INTEGER NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `preco` DECIMAL(65, 30) NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+    `lote` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Produto_codBarras_key`(`codBarras`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
